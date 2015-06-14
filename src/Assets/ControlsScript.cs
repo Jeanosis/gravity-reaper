51c775bd67a443b22851e46348ed03c7fc76fd9d
Jeanosis <plaits123@gmail.com>
2015-06-14 16:53:44 +0300

    force text added

--- src/Assets/ControlsScript.cs
+++ src/Assets/ControlsScript.cs
@@ -4,39 +4,25 @@ using System;
 
 public class ControlsScript : MonoBehaviour {
 	public KeyCode MoveLeft, MoveRight;
-	protected float Speed = 0.04F;
 	
 	private float time = 0;
-	public float amplitude = 1.6F, frequency = 2.0F, rad = (2.0F * Mathf.PI) / 360.0F, min = 100, max = -100;
+	private float amplitude = 1.6F, frequency = 2.0F, rad = (2.0F * Mathf.PI) / 360.0F, min = 100, max = -100;
 
 	void Start(){
 		transform.position = new Vector3 (0.9F, transform.position.y, transform.position.z);
-		//rigidbody2D.velocity = new Vector2((-frequency) * amplitude * Mathf.Sin (rad * (frequency * time /*+ 90*/)), 0);
-		//time = (float)Math.PI / frequency / Time.deltaTime / 2.0F;//Convert.ToInt32(Mathf.PI / frequency);
-		//Debug.Log (time);
 	}
 
 	void Update () {
 		if (Input.GetKey (MoveLeft)) {
-			rigidbody2D.velocity = new Vector2 (rigidbody2D.velocity.x - Speed, rigidbody2D.velocity.y);
+			amplitude += 0.05F;
 		} else if (Input.GetKey (MoveRight)) {
-			rigidbody2D.velocity = new Vector2 (rigidbody2D.velocity.x + Speed, rigidbody2D.velocity.y);
+			amplitude -= 0.05F;
 		}
 
 		var sin = Mathf.Sin (rad * (frequency * time));
-		/*if (sin > 0.95F || sin == -1 || sin == 0)
-			Debug.Log (String.Format("{0}, {1}, {2}", sin, transform.position.x, time));*/
-		//if (transform.position.x  > amplitude / 2 || transform.position.x < -amplitude / 2)
-		//	Debug.Log (String.Format("{0}, {1}, {2}", sin, transform.position.x, time));
 
-		rigidbody2D.velocity = new Vector2((-frequency) * amplitude * Mathf.Sin (rad * frequency * time), 0);
+		GetComponent<Rigidbody2D>().velocity = new Vector2((-frequency) * amplitude * Mathf.Sin (rad * frequency * time), 0);
 
-		if (transform.position.x < min)
-			min = transform.position.x;
-		if (transform.position.x > -min)
-			max = transform.position.x;
-		Debug.Log (String.Format("{0}, {1}", min, max));
-
-		time += 1;// / Time.deltaTime;*/
+		time += 1;
 	}
 }
--- src/Assets/GameSetup.cs
+++ src/Assets/GameSetup.cs
@@ -8,10 +8,10 @@ public class GameSetup : MonoBehaviour {
 
 	void Start () {
 		LeftWall.size = new Vector2 (1F, MainCamera.ScreenToWorldPoint (new Vector3 (0F, Screen.height * 2F, 0F)).y);
-		LeftWall.center = new Vector2 (-MainCamera.ScreenToWorldPoint (new Vector3 (Screen.width, 0F, 0F)).x - 0.5F, 0F);
+		LeftWall.offset = new Vector2 (-MainCamera.ScreenToWorldPoint (new Vector3 (Screen.width, 0F, 0F)).x - 0.5F, 0F);
 
 		RightWall.size = LeftWall.size;
-		RightWall.center = new Vector2 (-LeftWall.center.x, LeftWall.center.y);
+		RightWall.offset = new Vector2 (-LeftWall.offset.x, LeftWall.offset.y);
 
 		Hero.position = new Vector3 (Hero.position.x, MainCamera.ScreenToWorldPoint (new Vector3 (0F, 70F, 0F)).y, 0F);
 	}
