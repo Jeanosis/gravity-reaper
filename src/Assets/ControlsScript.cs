bbb39e909c928bd2591f4a9d683f85afec9d6501
Jeanosis <plaits123@gmail.com>
2015-06-18 23:42:25 +0300

    Sliders added

--- src/Assets/ControlsScript.cs
+++ src/Assets/ControlsScript.cs
@@ -1,12 +1,15 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System;
+using UnityEngine.UI;
 
 public class ControlsScript : MonoBehaviour {
 	public KeyCode MoveLeft, MoveRight;
-	
+	public Slider leftSlider, rightSlider;
+
 	private float time = 0;
-	private float amplitude = 1.6F, frequency = 2.0F, rad = (2.0F * Mathf.PI) / 360.0F, min = 100, max = -100;
+	private static float maxAmplitude = 1.6F;
+	private float amplitude = maxAmplitude / 2.0F, frequency = 2.0F, rad = (2.0F * Mathf.PI) / 360.0F, min = 100, max = -100;
 
 	void Start(){
 		transform.position = new Vector3 (0.9F, transform.position.y, transform.position.z);
@@ -24,5 +27,26 @@ public class ControlsScript : MonoBehaviour {
 		GetComponent<Rigidbody2D>().velocity = new Vector2((-frequency) * amplitude * Mathf.Sin (rad * frequency * time), 0);
 
 		time += 1;
+	}
+
+	public void OnLeftValueChanged()
+	{
+		/*rightSlider.value = 1.0F - leftSlider.value;
+		amplitude = maxAmplitude * leftSlider.value;*/
+		OnValueChanged (rightSlider, leftSlider);
+	}
+
+	public void OnRightValueChanged()
+	{
+		/*leftSlider.value = 1.0F - rightSlider.value;
+		amplitude = maxAmplitude * leftSlider.value;*/
+		OnValueChanged (leftSlider, rightSlider);
+	}
+
+	private void OnValueChanged(Slider first, Slider second)
+	{
+		first.value = 1.0F - second.value;
+		amplitude = maxAmplitude * first.value;
+
 	}
 }
--- src/Assets/GameSetup.cs
+++ src/Assets/GameSetup.cs
@@ -4,9 +4,14 @@ using System.Collections;
 public class GameSetup : MonoBehaviour {
 	public Camera MainCamera;
 	public Transform Hero;
+	public RectTransform leftSlider, rightSlider;
 	public BoxCollider2D LeftWall, RightWall;
 
 	void Start () {
+		var offset = 5.0F;
+		var width = (float)Screen.width;
+		var height = (float)Screen.height;
+
 		LeftWall.size = new Vector2 (1F, MainCamera.ScreenToWorldPoint (new Vector3 (0F, Screen.height * 2F, 0F)).y);
 		LeftWall.offset = new Vector2 (-MainCamera.ScreenToWorldPoint (new Vector3 (Screen.width, 0F, 0F)).x - 0.5F, 0F);
 
@@ -14,5 +19,13 @@ public class GameSetup : MonoBehaviour {
 		RightWall.offset = new Vector2 (-LeftWall.offset.x, LeftWall.offset.y);
 
 		Hero.position = new Vector3 (Hero.position.x, MainCamera.ScreenToWorldPoint (new Vector3 (0F, 70F, 0F)).y, 0F);
+
+		leftSlider.sizeDelta = new Vector2 (width / 2.0F - (2 * offset), 20);
+		leftSlider.position = new Vector3((leftSlider.sizeDelta.x / 2.0F) + offset, (leftSlider.sizeDelta.y / 2.0F) + offset);
+
+		rightSlider.sizeDelta = leftSlider.sizeDelta;
+		rightSlider.position = new Vector3(width - leftSlider.position.x, leftSlider.position.y);
+		//offset + (leftSlider.sizeDelta.x / 2.0F) - 0.5F, offset + (leftSlider.sizeDelta.y / 2.0F) - 0.5F);
+		//leftSlider.sca = new Vector2 (-MainCamera.ScreenToWorldPoint (new Vector3 (Screen.width, 0F, 0F)).x - 0.5F, 0F);
 	}
 }
